1b1d93ed8fe8ac958a14b37b0c03b09949e093ab
Dário Silva <dariosilva13222@gmail.com>
2023-09-24 12:50:34 +0000

    feat: conditionally renders buttons section

--- App.js
+++ App.js
@@ -33,10 +33,15 @@ export default function App() {
             selectedImage={selectedImage}
           />
       </View>
-      <View style={styles.footerContainer}>
+      {showAppOptions ? (
+          <View />
+      ) : (
+        <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
         <Button label="Use this photo" onPress={()=> setShowAppOptions(true)}/>
       </View>
+      )}
+      
       <StatusBar style="auto" />
     </View>
   );
