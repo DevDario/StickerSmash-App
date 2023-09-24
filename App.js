d0cccec54c777615b4926b296ab9508e4fe59f0f
Dário Silva <dariosilva13222@gmail.com>
2023-09-24 14:17:16 +0000

    chore: renders the EmojiList inside the EmojiPicker

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {useState} from 'react'
 import IconButton from './components/IconButton';
 import CircleButton from './components/CircleButton';
 import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
 
 const placeholderImage = require('./assets/images/background-image.png')
 
@@ -14,6 +15,7 @@ export default function App() {
   const [selectedImage, setSelectedImage] = useState(null)
   const [showAppOptions, setShowAppOptions] = useState(false)
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [pickedEmoji, setPickedEmoji] = useState(null)
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -68,7 +70,7 @@ export default function App() {
       </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-          {/*A list of emoji component will go here */}
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
     </View>
