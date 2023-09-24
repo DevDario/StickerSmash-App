57739bd3d8bbb363102ad1670560abe54384aede
Dário Silva <dariosilva13222@gmail.com>
2023-09-24 12:46:30 +0000

    chore: sets ShowAppOptions to true

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default function App() {
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
-        <Button label="Use this photo"/>
+        <Button label="Use this photo" onPress={()=> setShowAppOptions(true)}/>
       </View>
       <StatusBar style="auto" />
     </View>
